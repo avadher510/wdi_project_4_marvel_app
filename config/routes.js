3375dd72053e9b07c59da6df2e9b16720b843603
Anish Vadher <avadher510@gmail.com>
2018-02-27 13:21:40 +0000

    routes added to back end

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,22 @@
 const router = require('express').Router();
+const superheros  = require('../controllers/superheros');
+const auth  = require('../controllers/auth');
+const secureRoute = require('../lib/secureRoute');
 
-// routes go here
+router.route('/superheros')
+  .get(superheros.index)
+  .post(secureRoute, superheros.create);
+
+router.route('/superheros/:id')
+  .get(superheros.show)
+  .put(secureRoute, superheros.update)
+  .delete(secureRoute, superheros.delete);
+
+router.route('/register')
+  .post(auth.register);
+
+router.route('/login')
+  .post(auth.login);
 
 router.all('/*', (req, res) => res.notFound());
 
